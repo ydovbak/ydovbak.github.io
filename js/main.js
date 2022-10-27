1446cfff56f75bdd9825e75d8cdfc66819d13af3
Yuliia Dovbak <yulia.dovbak@gmail.com>
2022-10-27 20:40:11 +0100

    navbar fixed, android projects, minorupdates

--- js/main.js
+++ js/main.js
@@ -51,25 +51,25 @@ $(document).ready(function() {
   // ========================================================================= //
 
 
-  $(window).scroll(function() {
-    var scroll = $(window).scrollTop();
-    if (scroll > 200 ) {
-      $("#main-nav, #main-nav-subpage").slideDown(700);
-      $("#main-nav-subpage").removeClass('subpage-nav');
-    } else {
-      $("#main-nav").slideUp(700);
-      $("#main-nav-subpage").hide();
-      $("#main-nav-subpage").addClass('subpage-nav');
-    }
-  });
+  // $(window).scroll(function() {
+  //   var scroll = $(window).scrollTop();
+  //   if (scroll > 200 ) {
+  //     $("#main-nav, #main-nav-subpage").slideDown(700);
+  //     $("#main-nav-subpage").removeClass('subpage-nav');
+  //   } else {
+  //     $("#main-nav").slideUp(700);
+  //     $("#main-nav-subpage").hide();
+  //     $("#main-nav-subpage").addClass('subpage-nav');
+  //   }
+  // });
 
-  // ========================================================================= //
-  //  // RESPONSIVE MENU
-  // ========================================================================= //
+  // // ========================================================================= //
+  // //  // RESPONSIVE MENU
+  // // ========================================================================= //
 
-  $('.responsive').on('click', function(e) {
-    $('.nav-menu').slideToggle();
-  });
+  // $('.responsive').on('click', function(e) {
+  //   $('.nav-menu').slideToggle();
+  // });
 
   // ========================================================================= //
   //  Typed Js
